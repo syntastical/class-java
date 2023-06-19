7dc8ad4dbf30dac3f913a4e7cfc8d9020df54465
Sean Rester <syntastical@users.noreply.github.com>
2023-06-19 11:17:16 -0400

    add day1 hello world

--- day1/Example.java
+++ day1/Example.java
@@ -1,5 +1,12 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        var iAmString = "This is a string";
+        System.out.println(iAmString);
+        var iAmInt = 1;
+        System.out.println(iAmInt);
+        var iAmBool = true;
+        System.out.println(iAmBool);
     }
 }
